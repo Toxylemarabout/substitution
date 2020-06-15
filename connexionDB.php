d3a51e8d700ea3e8eb0813c4302c7e73cb0eb550
Antoine <antoine.vedier@viacesi.fr>
2020-06-15 18:35:19 +0200

    script connexion BDD (attentions aux identifiants)

---
+++ connexionDB.php
@@ -0,0 +1,14 @@
+ <?php
+
+ $dbUser = 'root';
+ $dbPass = '';
+ 
+ try {
+ 	$dbh = new PDO('mysql:host=localhost;dbname=substitution', $dbUser, $dbPass/*, array(PDO::ATTR_PERSISTENT => true)*/);
+ 	} 
+  catch (PDOException $e) {
+ 	print "Erreur de connexion à la base de données!: " . $e->getMessage() . "<br/>";
+ 	die();
+    }
+
+?> 
