40bf6507c734ff53e8d087f3c3ea23fbda9bf46e
Antoine <antoine.vedier@viacesi.fr>
2020-06-17 17:58:17 +0200

    fonctionnalité d'ajout d'utilisateur

---
+++ ajoutUtilisateur.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Ajout d'utilisateur</title>
+    </head>
+
+    <body>    
+
+    <div id="corps">
+
+         <?php include("header.php"); ?>
+
+        <div class="container">
+            
+            <section>               
+
+                <div id="container" >
+
+                    <div id="wrapper">
+
+
+                        
+                                <div id="login" class="animate form">
+                                <form method="post" action="scriptAjoutUtilisateur.php" autocomplete="on">
+                                    <h1>Inscription</h1> 
+                                    <p> 
+                                        <label for="username" class="uname" data-icon="u" > Identifiant : </label>
+                                        <input id="username" name="identifiant" required="required" type="text" minlength="2" required pattern="([A-z0-9À-ž\s]){2,}" />
+                                    </p>
+                                    <p> 
+                                        <label for="password" class="youpasswd" data-icon="p"> Mot de passe : </label>
+                                        <input id="password" name="password" required="required" type="password" minlength="4"/> 
+                                    </p>
+                                        <label for="password" class="youpasswd" data-icon="p"> Confirmation du mot de passe : </label>
+                                        <input id="confirm_password" name="password" required="required" type="password" minlength="4" /> 
+                                    <p>
+
+                                    <label for="statutAdmin">Statut administrateur</label>
+
+                                    <select name="statutAdmin" id="statutAdmin" >
+                                         <option value="0">Non</option>
+                                         <option value="1">Oui</option>
+                                     </select> 
+
+                                    </p>
+
+                                    <p class="login button"> 
+                                        <input type="submit" value="S'inscire"/> 
+                                    </p>
+
+                                </form>
+                             </div>
+                        
+                    </div>
+                </div>  
+            </section>
+        </div>
+
+        
+    </div>
+    
+    </body>
+
+</html>
---
+++ scriptAjoutUtilisateur.php
@@ -0,0 +1,40 @@
+<?php
+
+include 'connexionDB.php';
+
+if (isset($_POST['identifiant'])) $identifiant = $_POST['identifiant'];
+else      $identifiant = "";
+if (isset($_POST['password'])) $mdp = $_POST['password'];
+else      $mdp = "";
+if (isset($_POST['statutAdmin'])) $statutAdmin = $_POST['statutAdmin'];
+else      $statutAdmin = $_POST['statutAdmin'];
+
+//Sécurité, mais le formulaire est censé empêcher les envois vides
+if (empty($mdp)) {
+    echo '<script> alert("Veuillez entrer un mot de passe");</script>';
+} else if (empty($identifiant)) {
+    echo '<script> alert("Veuillez entrer un identifiant");</script>';
+
+} else {
+
+    try {
+        $req = $dbh->prepare('SELECT identifiant FROM utilisateurs WHERE identifiant = :identifiant');
+        $req->bindParam(':identifiant', $identifiant);
+        $req->execute();
+
+        if ($req->fetch()['identifiant']== $identifiant) {
+            echo "<script>alert('identifiant déjà utilisé'); window.location = './ajoutUtilisateur.php';</script>";
+        } else {
+            $req = $dbh->prepare("INSERT INTO utilisateurs (password, identifiant, statutAdmin) VALUES( :mdp, :identifiant, :statutAdmin)");
+            $req->bindParam(':mdp', $mdp);
+            $req->bindParam(':identifiant', $identifiant);
+            $req->bindParam(':statutAdmin', $statutAdmin);
+            $req->execute();
+            echo "<script>alert('ajout effectué'); window.location = './ajoutUtilisateur.php';</script>";
+        }
+    } catch (Exception $e){
+        echo '<script> alert("execution de la requette impossible");</script>';
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+?>
